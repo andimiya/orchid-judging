228248c2737885cea2b56e10763808cad2d78cd3
Andrea Takamiya <takamiya.andrea@gmail.com>
2017-11-10 15:09:36 -1000

    Changed to the currency name rather than id

--- src/components/TransactionTable.js
+++ src/components/TransactionTable.js
@@ -16,7 +16,7 @@ const TransactionTable = (props) => {
           return (
             <tr key={transactions.transaction_id}>
               <td key={transactions.updated_at}>{moment.utc(transactions.updated_at).format('MMM DD, YYYY - hh:mm a')}</td>
-              <td>{transactions.crypto_type_id}</td>
+              <td>{transactions.name}</td>
               <td>{transactions.usd_invested}</td>
               <td>{transactions.coin_purchased}</td>
             </tr>
