8e1572d9047830ca979c9d37a6feca1381c64872
Andrea Takamiya <takamiya.andrea@gmail.com>
2017-11-13 09:50:28 -1000

    Updated package json to remove the homepage setting

--- deploy.sh
+++ deploy.sh
@@ -2,4 +2,7 @@
 
 npm run build
 
-aws s3 sync build/ s3://cryptotracker --delete --grants read=uri=http://acs.amazonaws.com/groups/global/AllUsers
+aws s3 sync build/ s3://andimiya-crypto-app --delete --grants read=uri=http://acs.amazonaws.com/groups/global/AllUsers
+
+
+# aws s3 sync build/ s3://cryptotracker --delete --grants read=uri=http://acs.amazonaws.com/groups/global/AllUsers
