b57749866ce63d9517bf6e7a1b5ab1bab2eda35d
Andrea Takamiya <takamiya.andrea@gmail.com>
2018-03-09 18:33:04 -0800

    Bootstrapped create account form

--- src/components/CreateAccountForm.js
+++ src/components/CreateAccountForm.js
@@ -4,31 +4,45 @@ class CreateAccountForm extends Component {
   render() {
     const { handleRegistration } = this.props;
     return (
-      <div className="register__form">
-        <h1 className="section__title">Account Details</h1>
+      <div className="register__form outer" >
+        <h1 className="section__title">Create an Account</h1>
         <div className="form__error">
           <p>{this.props.error}</p>
         </div>
         <form onSubmit={handleRegistration}>
-          <label className="form__field account-form__field">
-            <span className="form__label-text account-form__label-text">Email Address</span>
-            <input className="form__input account-form__input" name="email" type="text" required />
-          </label>
-          <label className="form__field account-form__field">
-            <span className="form__label-text account-form__label-text">Name</span>
-            <input className="form__input account-form__input" name="firstName" type="text" required />
-          </label>
-          <label className="form__field account-form__field">
-            <span className="form__label-text account-form__label-text">Password<br />
-              <span className="form__label-text account-form__subtext">Minimum 8 characters, upper and lower case</span>
-            </span>
-            <input className="form__input account-form__input" name="passwordOne" type="password" required />
-          </label>
-          <label className="form__field account-form__field">
-            <span className="form__label-text account-form__label-text">Retype Password</span>
-            <input className="form__input account-form__input" name="passwordTwo" type="password" required />
-          </label>
-          <button className="button button--form"type="submit">Create Account</button>
+          <div className="form-group">
+            <input 
+              type="text"
+              name="email"
+              placeholder="Email Address"
+              className="form-control"
+              required
+            />
+            <input 
+              type="text"
+              name="firstName"
+              placeholder="First Name"
+              className="form-control"
+              required
+            />
+            <label>Minimum 8 characters, upper and lower case</label>
+            <input 
+              type="password"
+              name="passwordOne"
+              placeholder="Password"
+              className="form-control"
+              required
+            />
+            <label>Minimum 8 characters, upper and lower case</label>
+            <input 
+              type="password"
+              name="passwordOne"
+              placeholder="Re-enter password"
+              className="form-control"
+              required
+            />
+            <button className="button button--form"type="submit">Create Account</button>
+          </div>
         </form>
       </div>
     );
