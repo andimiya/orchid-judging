0db7edf5398131569d0f4cdaa6c541c9b1a749bd
Andrea Takamiya <takamiya.andrea@gmail.com>
2018-03-14 22:46:33 -0700

    Added a marketing page for the homepage

---
+++ src/containers/MktgPageContainer/index.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import Page from '../../components/Page';
+
+class MktgPageContainer extends Component {
+  
+  render() {
+    return (
+      <Page>
+        <div className="outer">
+          <h1>Crypto Tracker</h1>
+          <p>Track your shit</p>
+        </div>
+      </Page>
+    );
+  }
+}
+
+export default MktgPageContainer;
